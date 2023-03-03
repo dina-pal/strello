a3f7a64baba920c7b5a020196bb6b326777fd2fd
Dina Pal <dinapal.dev@gmail.com>
2023-03-03 18:44:05 +0530

    Update Auth  functionality on Server.

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const morgan = require('morgan');
 const cors = require('cors');
 module.exports.middleware = [
@@ -6,3 +7,22 @@ module.exports.middleware = [
     morgan('dev'),
     cors()
 ];
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns {Object} Token Payload {payload} will be returned. 
+ */
+module.exports.authMiddleware = async (req, res, next) => {
+    const token = req.cookies.jwt_sign_token;
+    if(token === undefined) {
+       return res.redirect('/home')
+    }else{
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.payload = payload;
+        next();
+    }  
+    
+};
